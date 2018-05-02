0d2d93ef8a76608092d4718cbcf6bb77428bd92c
Bryan Purakal <bpurakal@gmail.com>
2018-05-01 22:19:30 -0400

    first commit

---
+++ src/com/semanticsquare/currencyConverter/CurrencyConverter.java
@@ -0,0 +1,20 @@
+public class CurrencyConverter {
+
+  public static void main (String[] args){
+
+      int rupee = 63;
+      int dirham = 3; //UAE
+      int real = 3; //brazilian
+
+        chilean_peso = 595;
+        mexican_peso = 18;
+
+        _yen = 107;
+        $australian = 2;  // australian dollar
+        dollar;
+        Rupee = 63;
+
+        }
+
+
+}
